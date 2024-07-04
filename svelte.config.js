e1c2c5d85590d9bcc5a2cab607a145bdb7cd1007
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-07-05 04:36:24 +0800

    Format code and ignore target folder

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,9 +12,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: "app.html",
-		}),
-	},
+			fallback: 'app.html'
+		})
+	}
 };
 
 export default config;
